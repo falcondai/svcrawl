6206906c5004c74f5fd049f36672ceeb6c8a030d
Falcon Dai <me@falcondai.com>
2014-03-17 12:38:24 -0500

    geo point -> point, (long,lat) convention corrected

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 class Pano(me.Document):
     location = me.StringField()
-    latlong = me.GeoPointField()
+    longlat = me.PointField(auto_index=True) # note that the coordinates are (long, lat) pairs
     heading = me.FloatField(default=None)
     fov = me.FloatField(default=90)
     pitch = me.FloatField(default=0)
@@ -19,9 +19,13 @@ class Pano(me.Document):
     pano_id = me.StringField()
     image = me.ImageField()
 
+    meta = {
+        'indexes': ['pano_id']
+    }
+
     @property
     def url(self):
-        loc = self.location or self.latlong
+        loc = self.location or self.longlat
         return generate_pano_url(loc, self.heading, self.fov, self.pitch, self.size, self.pano_id)
     
     @property
@@ -33,22 +37,30 @@ class Pano(me.Document):
 
     @property
     def PIL_image(self):
-        if not hasattr(self, '_PIL_image'):
-            self._PIL_image = Image.open(cStringIO.StringIO(self.image.read()))
-        return self._PIL_image
+        return Image.open(cStringIO.StringIO(self.image.read()))
 
     def show_image(self):
         return self.PIL_image.show()
 
     def __repr__(self):
-        return '<Pano: location=%r, latlong=%r, image_md5=%r>' % (self.location, self.latlong, self.image_md5)
+        return '<Pano: location=%r, longlat=%r, image_md5=%r>' % (self.location, self.longlat, self.image_md5)
+
+    @staticmethod
+    def new_pano(location=None, heading=None, fov=90, pitch=0, size=(640, 640), pano_id=None, key=None):
+        image = cStringIO.StringIO(get_pano(location, heading, fov, pitch, size, pano_id, key))
+        if isinstance(location, str):
+            pano = Pano(location=location, heading=heading, fov=fov, pitch=pitch, size=size, pano_id=pano_id, image=image)
+        else:
+            # location is provided as a (long, lat) pair
+            pano = Pano(longlat=location, heading=heading, fov=fov, pitch=pitch, size=size, pano_id=pano_id, image=image)
+        return pano
 
 
 api_base = 'http://maps.googleapis.com/maps/api/streetview'
 
 def generate_pano_url(location=None, heading=None, fov=90, pitch=0, size=(640, 640), pano_id=None, key=None):
     params = {
-        'location': location if isinstance(location, str) else '%f,%f' % location,
+        'location': location if isinstance(location, str) else '%f,%f' % (location[1], location[0]), # assume (long, lat) pair
         'pano': pano_id,
         'heading': heading % 360 if heading else None, # heading will be pointing to the street address if None
         'fov': min(fov, 120),
@@ -73,22 +85,15 @@ def get_pano(location=None, heading=None, fov=90, pitch=0, size=(640, 640), pano
     return r.content
 
 
-def new_pano(location=None, heading=None, fov=90, pitch=0, size=(640, 640), pano_id=None, key=None):
-    image = cStringIO.StringIO(get_pano(location, heading, fov, pitch, size, pano_id, key))
-    if isinstance(location, str):
-        pano = Pano(location=location, heading=heading, fov=fov, pitch=pitch, size=size, pano_id=pano_id, image=image)
-    else:
-        # location is provided as a latlong pair
-        pano = Pano(latlong=location, heading=heading, fov=fov, pitch=pitch, size=size, pano_id=pano_id, image=image)
-    return pano
-
-
 if __name__ == '__main__':
-    print generate_pano_url('Chicago')
+    print 'testing...'
+    print generate_pano_url((-0.0004797, 51.4769351))
 
     db = me.connect('streetview')
-    for l in [(0,0), (180, 30), 'Chicago', 'San Francisco', 'London', 'Berlin', 'New York', 'Washington DC']:
-        p = new_pano(l)
+    for l in [(-0.0004797, 51.4769351), 'downtown chicago', 'Golden Gate Bridge', 'Big Ben', 'Empire State Building', 'White House']:
+        p = Pano.new_pano(l, fov=120)
+        p.show_image()
         print repr(p)
         p.save()
+        p.delete()
 
